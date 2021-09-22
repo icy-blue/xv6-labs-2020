de3eaa34179d5278fef3e572b2b73cc411177c14
icy <i@icys.top>
2021-09-22 23:54:35 +0800

    fix user.h

--- user/user.h
+++ user/user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int trace(int);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
@@ -40,4 +40,3 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
-int trace(int);
