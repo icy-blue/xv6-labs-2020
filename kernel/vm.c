4ea54cbebd54d934ec18c9fc07895ec269e6d6cc
icy <i@icys.top>
2021-10-11 17:03:40 +0800

    finish lab

--- kernel/vm.c
+++ kernel/vm.c
@@ -181,9 +181,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+//      panic("uvmunmap: walk");
+        continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+//      panic("uvmunmap: not mapped");
+        continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,10 +317,13 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+//      panic("uvmcopy: pte should exist");
+          continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+//      panic("uvmcopy: page not present");
+          continue;
     pa = PTE2PA(*pte);
+
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
       goto err;
@@ -359,8 +364,10 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
+    if(pa0 == 0) {
+        if (handle_page(va0, myproc()) != -1) pa0 = walkaddr(pagetable, va0);
+        else return -1;
+    }
     n = PGSIZE - (dstva - va0);
     if(n > len)
       n = len;
@@ -384,8 +391,10 @@ copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
   while(len > 0){
     va0 = PGROUNDDOWN(srcva);
     pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
+    if(pa0 == 0) {
+        if (handle_page(va0, myproc()) != -1) pa0 = walkaddr(pagetable, va0);
+        else return -1;
+    }
     n = PGSIZE - (srcva - va0);
     if(n > len)
       n = len;
@@ -411,8 +420,10 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   while(got_null == 0 && max > 0){
     va0 = PGROUNDDOWN(srcva);
     pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
+    if(pa0 == 0) {
+        if (handle_page(va0, myproc()) != -1) pa0 = walkaddr(pagetable, va0);
+        else return -1;
+    }
     n = PGSIZE - (srcva - va0);
     if(n > max)
       n = max;
