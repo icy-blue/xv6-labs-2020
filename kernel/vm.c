7456a7446bd52640aa0fa66b0ba24f6174132210
icy-blue <i@icys.top>
2021-10-27 00:32:09 +0800

    Cow2 (#1)
    * Revert "changed"
    
    This reverts commit 186197eb2554eb01dbc5267212d0a7d4c2ab7d28.
    
    * Update kalloc.c
    
    * vm
    
    * upd
    
    * Update kalloc.c

--- kernel/vm.c
+++ kernel/vm.c
@@ -320,11 +320,12 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    if(flags & PTE_W) {
+        flags = (flags & ~PTE_W) | PTE_COW;
+        *pte = PA2PTE(pa) | flags;
+    }
+    inc_rc(pa);
+    if(mappages(new, i, PGSIZE, pa, flags) != 0){
       goto err;
     }
   }
@@ -358,6 +359,7 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+    cow_alloc(pagetable, va0);
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
