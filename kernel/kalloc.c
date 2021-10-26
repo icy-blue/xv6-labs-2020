186197eb2554eb01dbc5267212d0a7d4c2ab7d28
icy <i@icys.top>
2021-10-26 23:09:36 +0800

    changed

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -35,8 +35,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+      refs[get_ref(p)] = 0;
+      kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +53,18 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  sub_ref(pa);
+  if(get_ref(pa) == 0) {
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run *) pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -76,7 +81,9 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  if(r) {
+      memset((char *) r, 5, PGSIZE); // fill with junk
+      refs[get_ref(r)] = 1;
+  }
   return (void*)r;
 }
--- user/cowtest.c
+++ user/cowtest.c
@@ -180,6 +180,7 @@ filetest()
 int
 main(int argc, char *argv[])
 {
+    printf("cowtest");
   simpletest();
 
   // check that the first simpletest() freed the physical memory.
